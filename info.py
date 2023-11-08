60b87144a4446019ab9d2e854ce7fe665165ab33
Vikas-841 <119723483+Vikas-841@users.noreply.github.com>
2023-11-08 11:36:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
+# Don't Remove Credit @kira_botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @Kira_developer
 
 
 import re
